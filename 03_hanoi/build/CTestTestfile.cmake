49521f3eaca78d8886c812f53116f587859d07e4
Chaos <85056161+allmightychaos@users.noreply.github.com>
2023-03-02 15:34:06 +0100

    Deleting build folder

--- 03_hanoi/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/chaos/Personal/Schule/2022:23/POS/Repositories/09 Hanoi/03_hanoi
-# Build directory: /Users/chaos/Personal/Schule/2022:23/POS/Repositories/09 Hanoi/03_hanoi/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
