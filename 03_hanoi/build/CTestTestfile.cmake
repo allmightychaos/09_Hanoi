e32d65925c44415622e0c07d8fe0001c69bd1459
Chaos <85056161+allmightychaos@users.noreply.github.com>
2023-03-02 12:58:02 +0100

    Added the last extension, for version 3.0

---
+++ 03_hanoi/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/chaos/Personal/Schule/2022:23/POS/Repositories/09 Hanoi/03_hanoi
+# Build directory: /Users/chaos/Personal/Schule/2022:23/POS/Repositories/09 Hanoi/03_hanoi/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
