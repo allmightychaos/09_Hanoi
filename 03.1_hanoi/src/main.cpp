0fab4a7e6b650cd1ac9e0f56588b734580eaf740
Chaos <85056161+allmightychaos@users.noreply.github.com>
2023-03-03 10:14:42 +0100

    Added the time frame in which it should update

--- 03.1_hanoi/src/main.cpp
+++ 03.1_hanoi/src/main.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-void move(Tower &start, Tower &ziel, Tower &zwischenspeicher, int anzahl_der_scheiben, int anzahl_bewegungen, int &counter, Tower &t1, Tower &t2, Tower &t3)
+void move(Tower &start, Tower &ziel, Tower &zwischenspeicher, int anzahl_der_scheiben, int anzahl_bewegungen, int &counter, Tower &t1, Tower &t2, Tower &t3, int zeit)
 {
     if (anzahl_der_scheiben == 1) {
         ziel.push(start.pop());
@@ -16,11 +16,11 @@ void move(Tower &start, Tower &ziel, Tower &zwischenspeicher, int anzahl_der_sch
             t2.print(13, 0);
             t3.print(25, 0);
             cout << "\n\nAnzahl der Bewegungen: " << counter << endl;
-            this_thread::sleep_for(chrono::milliseconds(500));
+            this_thread::sleep_for(chrono::milliseconds(zeit));
         }
     }
     else {
-        move(start, zwischenspeicher, ziel, anzahl_der_scheiben - 1, anzahl_bewegungen, counter, t1, t2, t3);
+        move(start, zwischenspeicher, ziel, anzahl_der_scheiben - 1, anzahl_bewegungen, counter, t1, t2, t3, zeit);
         ziel.push(start.pop());
         counter++;
         if (counter % anzahl_bewegungen == 0) {
@@ -29,20 +29,31 @@ void move(Tower &start, Tower &ziel, Tower &zwischenspeicher, int anzahl_der_sch
             t2.print(13, 0);
             t3.print(25, 0);
             cout << "\n\nAnzahl der Bewegungen: " << counter << endl;
-            this_thread::sleep_for(chrono::milliseconds(500));
+            this_thread::sleep_for(chrono::milliseconds(zeit));
         }
-        move(zwischenspeicher, ziel, start, anzahl_der_scheiben - 1, anzahl_bewegungen, counter, t1, t2, t3);
+        move(zwischenspeicher, ziel, start, anzahl_der_scheiben - 1, anzahl_bewegungen, counter, t1, t2, t3, zeit);
     }
 }
 
 int main(int argc, char** argv) {
+    system("clear");
     int anzahl_bewegungen;
+    int zeit;
+
     if (argc < 2) {
         cout << "Bitte geben Sie die Anzahl der Bewegungen an.\n> ";
         cin >> anzahl_bewegungen;
+        cout << "Bitte geben Sie die Zeit in Millisekunden an.\n> ";
+        cin >> zeit;
+    }
+    else if (argc = 2) {
+        anzahl_bewegungen = stoi(argv[1]);
+        cout << "Bitte geben Sie die Zeit in Millisekunden an.\n> ";
+        cin >> zeit;
     }
     else {
         anzahl_bewegungen = stoi(argv[1]);
+        zeit = stoi(argv[2]);
     }
 
     Tower t1("Start", 8, true);
@@ -55,7 +66,7 @@ int main(int argc, char** argv) {
 
     int counter = 1;
     try {
-        move(t1, t3, t2, 8, anzahl_bewegungen, counter, t1, t2, t3);
+        move(t1, t3, t2, 8, anzahl_bewegungen, counter, t1, t2, t3, zeit);
     }
     catch(const std::runtime_error& e) {
         std::cerr << e.what() << '\n';
